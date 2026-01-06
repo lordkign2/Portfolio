8c726eedca26ccb8fbeb341277cb0d5ddc016927
lordkign2 <Lordkign2@gmail.com>
2026-01-06 04:39:29 +0100

    sitemap fix

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -2,30 +2,30 @@ import { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
     const baseUrl = 'https://umeh-kingsley-portfolio.netlify.app';
-    const currentDate = new Date();
+    const lastModified = new Date().toISOString().split('T')[0]; // YYYY-MM-DD
 
     return [
         {
-            url: baseUrl,
-            lastModified: currentDate,
+            url: `${baseUrl}/`,
+            lastModified: lastModified,
             changeFrequency: 'weekly',
             priority: 1,
         },
         {
             url: `${baseUrl}/about`,
-            lastModified: currentDate,
+            lastModified: lastModified,
             changeFrequency: 'monthly',
             priority: 0.8,
         },
         {
             url: `${baseUrl}/projects`,
-            lastModified: currentDate,
+            lastModified: lastModified,
             changeFrequency: 'weekly',
             priority: 0.9,
         },
         {
             url: `${baseUrl}/contact`,
-            lastModified: currentDate,
+            lastModified: lastModified,
             changeFrequency: 'yearly',
             priority: 0.7,
         },
